0bdab713ce4928db16be92f1e2b8531505559747
afzal-2004 <moa44468@gmail.com>
2024-07-24 09:24:42 +0530

    Home page issue

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,10 @@
 /* eslint-disable react/prop-types */
-import { CiSearch } from "react-icons/ci";
 import { FiPlus } from "react-icons/fi";
 import { useState, useEffect } from "react";
 import "./components.css";
 import { collection, onSnapshot } from "firebase/firestore";
 import { Db } from "../config/firebase";
-import FirebaseImage from "../../public/logos_firebase.svg";
+import FirebaseImage from "/logos_firebase.svg";
 
 import { Form } from "./Form";
 
@@ -38,30 +37,6 @@ const Home = ({ contacts, setcontacts }) => {
     fetchdata();
   }, [contacts, setcontacts]);
 
-  const SearchContact = (e) => {
-    const ContactSearch = e.target.value;
-    // console.log(ContactSearch);
-    const Contactref = collection(Db, "contact");
-
-    onSnapshot(Contactref, (snapshot) => {
-      const contactList = snapshot.docs.map((doc) => {
-        return {
-          id: doc.id,
-          ...doc.data(),
-        };
-      });
-      console.log(contactList);
-
-      const filterdData = contactList.filter((contact) =>
-        contact.name.toLowerCase().includes(ContactSearch.toLowerCase())
-      );
-      console.log(filterdData);
-      setcontacts(filterdData);
-      // console.log(contacts);
-      return filterdData;
-    });
-  };
-
   return (
     <>
       <main className="">
@@ -70,18 +45,8 @@ const Home = ({ contacts, setcontacts }) => {
           <p> Firebase Contact App</p>
         </section>
         <section className=" flex mt-4  w-full gap-3">
-          <div className=" flex border-2 border-white  w-full justify-start items-center rounded-xl gap-1">
-            <CiSearch className="  text-[35px]  text-white " />
-
-            <input
-              onChange={SearchContact}
-              type="Search Contact"
-              className="w-full bg-transparent border-none p-3 text-white  border-b-black border text-[20px]"
-            />
-          </div>
-
           <FiPlus
-            className="  text-[40px]  text-black bg-white   rounded-full"
+            className="  text-[40px]  text-black bg-white   rounded-full text-center"
             onClick={isOpenModel}
           />
         </section>
